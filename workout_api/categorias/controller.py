c0f9bd1fbeea1e3aaad25ed02211aa3714b13f6a
NicolasGayb <nicolasgbleal@gmail.com>
2025-10-23 09:26:51 -0300

    finalização desafio DiO

--- workout_api/categorias/controller.py
+++ workout_api/categorias/controller.py
@@ -53,3 +53,41 @@ async def query(id: UUID4, db_session: DataBaseDependency) -> CategoriaOut:
     if not categoria:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Categoria não encontrada com o ID {id}.')
     return categoria
+
+@router.delete(
+    '/{categoria_id}',
+    summary='Deletar uma categoria pelo ID',
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+async def delete(id: UUID4, db_session: DataBaseDependency) -> None:
+    categoria: CategoriaModel = (await db_session.execute(select(CategoriaModel).filter_by(id=id))).scalars().first()
+
+    if not categoria:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Categoria não encontrada com o ID {id}.')
+
+    await db_session.delete(categoria)
+    await db_session.commit()
+
+@router.patch(
+    '/{categoria_id}',
+    summary='Editar uma categoria pelo ID',
+    status_code=status.HTTP_200_OK,
+    response_model=CategoriaOut,
+)
+async def query(id: UUID4, db_session: DataBaseDependency, categoria_in: CategoriaIn = Body(...)) -> CategoriaOut:
+    categoria: CategoriaOut = (await db_session.execute(select(CategoriaModel).filter_by(id=id))).scalars().first()
+
+    if not categoria:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Categoria não encontrada com o ID {id}.')
+
+    elif categoria_in.nome and categoria_in.nome == categoria.nome:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='O nome da categoria não pode ser o mesmo.')
+
+    categoria_update = categoria_in.model_dump(exclude_unset=True)
+    for key, value in categoria_update.items():
+        setattr(categoria, key, value)
+
+    await db_session.commit()
+    await db_session.refresh(categoria)
+
+    return categoria
--- workout_api/centro_treinamento/controller.py
+++ workout_api/centro_treinamento/controller.py
@@ -5,6 +5,7 @@ from workout_api.centro_treinamento.models import CentroTreinamentoModel
 from workout_api.centro_treinamento.schemas import CentroTreinamentoIn, CentroTreinamentoOut
 from workout_api.contrib.dependencies import DataBaseDependency
 from sqlalchemy.future import select
+from uuid import UUID as _UUID
 
 router = APIRouter()
 
@@ -42,14 +43,56 @@ async def query(db_session: DataBaseDependency) -> list[CentroTreinamentoOut]:
     return centros
 
 @router.get(
-    '/{categoria_id}',
+    '/{ct_id}',
     summary='Listar um centro de treinamento pelo ID',
     status_code=status.HTTP_200_OK,
     response_model=CentroTreinamentoOut,
 )
-async def query(id: UUID4, db_session: DataBaseDependency) -> CentroTreinamentoOut:
+async def query(ct_id: UUID4, db_session: DataBaseDependency) -> CentroTreinamentoOut:
     centro: CentroTreinamentoOut = (await db_session.execute(select(CentroTreinamentoModel).filter_by(id=id))).scalars().first()
 
     if not centro:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Centro de treinamento não encontrado com o ID {id}.')
+    return centro
+
+@router.delete(
+    '/{ct_id}',
+    summary='Deletar um centro de treinamento pelo ID',
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+async def delete(ct_id: UUID4, db_session: DataBaseDependency) -> None:
+    centro: CentroTreinamentoModel = (await db_session.execute(select(CentroTreinamentoModel).filter_by(id=ct_id))).scalars().first()
+
+    if not centro:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Centro de treinamento não encontrado com o ID {ct_id}.')
+    try:
+        _UUID(str(ct_id), version=4)
+    except ValueError:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail='O ID deve estar no padrão correto (ex: 123e4567-e89b-12d3-a456-426614174000).')
+
+    await db_session.delete(centro)
+    await db_session.commit()
+
+@router.patch(
+    '/{ct_id}',
+    summary='Editar um centro de treinamento pelo ID',
+    status_code=status.HTTP_200_OK,
+    response_model=CentroTreinamentoOut,
+)
+async def query(ct_id: UUID4, db_session: DataBaseDependency, centro_treinamento_in: CentroTreinamentoIn = Body(...)) -> CentroTreinamentoOut:
+    centro: CentroTreinamentoOut = (await db_session.execute(select(CentroTreinamentoModel).filter_by(id=ct_id))).scalars().first()
+
+    if not centro:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Centro de treinamento não encontrado com o ID {ct_id}.')
+    elif centro_treinamento_in.nome and centro_treinamento_in.nome == centro.nome:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='O nome do centro de treinamento não pode ser o mesmo.')
+    
+
+    centro_update = centro_treinamento_in.model_dump(exclude_unset=True)
+    for key, value in centro_update.items():
+        setattr(centro, key, value)
+
+    await db_session.commit()
+    await db_session.refresh(centro)
+
     return centro
